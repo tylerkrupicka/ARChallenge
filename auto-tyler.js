2199222a00d26dac80b2beb86a6bb5949406e83e
tkrupicka <tylerkrupicka@gmail.com>
2015-04-28 23:16:25 -0400

    added buffering, generalized classify function, added george cascade xml

--- auto-tyler.js
+++ auto-tyler.js
@@ -1,77 +1,100 @@
 var arDrone = require('ar-drone');
 var cv = require('opencv');
 var http    = require('http');
-//var fs = require('fs');
 
 console.log('Connecting NimbustheGreat png stream ...');
 
-//var stream  = arDrone.createUdpNavdataStream();
 var client = arDrone.createClient();
 var pngStream = client.getPngStream();
 var processingImage = false;
 var lastPng;
 var navData;
 var flying = false;
-var found = false;
 var startTime = new Date().getTime();
 var log = function(s){
-var time = ( ( new Date().getTime() - startTime ) / 1000 ).toFixed(2);
-
-  console.log(time+" \t"+s);
+    var time = ( ( new Date().getTime() - startTime ) / 1000 ).toFixed(2);
+    console.log(time+" \t"+s);
 }
+
+//init buffers
+var georgeArray = [];
+var janeArray = [];
+var judyArray = [];
+var elroyArray = [];
+//init detection booleans
+var georgeFound = false;
+var janeFound = false;
+var judyFound = false;
+var elroyFound = false;
 
 //initialize PNG stream
 pngStream
-  .on('error', console.log)
-  .on('data', function(pngBuffer) {
+.on('error', console.log)
+.on('data', function(pngBuffer) {
     //console.log("got image");
     lastPng = pngBuffer;
-  });
+});
 
 //Face Detection Method using Cascades.
-//
-//
-  var detectGeorge = function(name,cascade,buffer,size){
-      if( found ) return
-      if( ( ! processingImage ) && lastPng )
-      {
+//Parameters: name of character, cascade file, buffer array, size of buffer, bool.
+var detectGeorge = function(name,cascade,buffer,size,foundBool){
+    //make sure we are not processing an image
+    if( ( ! processingImage ) && lastPng ){
         processingImage = true;
+        //read the image
         cv.readImage( lastPng, function(err, im) {
-          var opts = {};
-          im.detectObject(GEORGEY.xml, opts, function(err, faces) {
+            var opts = {};
+            im.detectObject(cascade, opts, function(err, found) {
+                //buffering
+                //push new length
+                buffer.unshift(found.length);
+                if(buffer.length >= size){
+                    //make sure we dont add too many
+                    buffer.pop();
+                }
+                //loop through and check if we found them
+                var f;
+                var count = 0;
+                for(var k = 0; k < found.length; k++) {
+                    f = found[k];
+                    if(f >= 1){
+                        count = count + 1;
+                    }
+                }
+                if(count = found.length){
+                    foundBool = true;
+                    //log diagnostics
+                    log('%s: found! | buffer:%d/%d',name,count,size)
+                }
+                else{
+                    foundBool = false;
+                    //log diagnostics
+                    log('%s: not found | buffer:%d/%d',name,count,size)
+                }
 
-            var face;
-            var biggestFace;
+                processingImage = false;
 
-            for(var k = 0; k < faces.length; k++) {
-
-              //im.rectangle([face.x, face.y], [face.x + face.width, face.y + face.height], [0, 255, 0], 2);
-            }
-            log(faces.length)
-            processingImage = false;
-          //im.save('/tmp/salida.png');
-
-        }, opts.scale, opts.neighbors
-          , opts.min && opts.min[0], opts.min && opts.min[1]);
-
-      });
+            }, opts.scale, opts.neighbors
+            , opts.min && opts.min[0], opts.min && opts.min[1]);
+        });
     };
-  };
+};
 
-var faceIntervalGE = setInterval( detectGeorge, 150, );
-var faceIntervalJA = setInterval( detectJane, 150, );
-var faceIntervalEL = setInterval( detectElroy, 150, );
-var faceIntervalJU = setInterval( detectJudy, 150, );
+function george(){
+    detectGeorge("George", "georgeCascade.xml", georgeArray, 5, georgeFound);
+}
+var georgeInterval = setInterval(george, 150);
 
 //Actual Script
+/*
 client.takeoff();
 client.after(5000,function(){
-  log("going up");
-  //this.up(1);
+    log("going up");
+    //this.up(1);
 }).after(1000,function(){
-  log("stopping");
-  this.stop();
-  flying = true;
+    log("stopping");
+    this.stop();
+    flying = true;
 });
 
 
@@ -79,24 +102,24 @@ client.after(10000, function() {
     flying = false;
     this.stop();
     this.land();
-  });
-
+});
+*/
 client.on('navdata', function(navdata) {
-  navData = navdata;
+    navData = navdata;
 })
 
 
 var server = http.createServer(function(req, res) {
-  if (!lastPng) {
-    res.writeHead(503);
-    res.end('Did not receive any png data yet.');
-    return;
-  }
+    if (!lastPng) {
+        res.writeHead(503);
+        res.end('Did not receive any png data yet.');
+        return;
+    }
 
-  res.writeHead(200, {'Content-Type': 'image/png'});
-  res.end(lastPng);
+    res.writeHead(200, {'Content-Type': 'image/png'});
+    res.end(lastPng);
 });
 
 server.listen(8080, function() {
-  console.log('Serving latest png on port 8080 ...');
+    console.log('Serving latest png on port 8080 ...');
 });
